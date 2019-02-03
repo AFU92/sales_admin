87ba9bee04cba3e916312eb54d681ee609d0f98f
Andrea Fuentes <andreita_f92@hotmail.com>
2019-02-03 17:03:10 -0500

    products and customer crud

--- lib/sales_admin/inventories/inventories.ex
+++ lib/sales_admin/inventories/inventories.ex
@@ -1,7 +1,7 @@
 defmodule SalesAdmin.Inventories do
-  alias SalesAdmin.Inventories.{StoreType, Store}
+  alias SalesAdmin.Inventories.{StoreType, Store, PaymentType, Customer, Product, Sale}
   alias SalesAdmin.Repo
-
+  import Ecto.Query
   # --------------------------------StoresTypes--------------------------------
 
   def list_store_types do
@@ -45,5 +45,110 @@ defmodule SalesAdmin.Inventories do
 
   def delete_store(%Store{} = store) do
     Repo.delete(store)
+  end
+
+  # --------------------------------PaymentTypes--------------------------------
+
+  def list_payment_types do
+    Repo.all(PaymentType)
+  end
+
+  def get_payment_type(id) do
+    case Repo.get(PaymentType, id) do
+      nil -> {:error, "Payment type not found for the given id: #{id}"}
+      payment_type -> {:ok, payment_type}
+    end
+  end
+
+  # --------------------------------Customer--------------------------------
+
+  def list_customers do
+    Repo.all(Customer)
+  end
+
+  def get_customer(id) do
+    case Repo.get(Customer, id) do
+      nil -> {:error, "Customer not found for the given id: #{id}"}
+      customer -> {:ok, customer}
+    end
+  end
+
+  def create_customer(params) do
+    %Customer{}
+    |> Customer.changeset(params)
+    |> Repo.insert()
+  end
+
+  def update_customer(
+        %Customer{} = customer,
+        params
+      ) do
+    customer
+    |> Customer.changeset(params)
+    |> Repo.update()
+  end
+
+  def delete_customer(%Customer{} = customer) do
+    Repo.delete(customer)
+  end
+
+  # --------------------------------Product--------------------------------
+
+  def list_products(store_id) do
+    from(p in Product, where: p.store_id == ^store_id)
+    |> Repo.all()
+  end
+
+  def get_product(store_id, id) do
+    result =
+      from(p in Product, where: p.store_id == ^store_id)
+      |> Repo.get(id)
+
+    case result do
+      nil -> {:error, "Product not found for the given id: #{id}"}
+      product -> {:ok, product}
+    end
+  end
+
+  def list_products_by_name(store_id, product_name) do
+    from(p in Product, where: p.store_id == ^store_id and like(p.name, ^product_name))
+    |> Repo.all()
+  end
+
+  def create_product(params) do
+    %Product{}
+    |> Product.changeset(params)
+    |> Repo.insert()
+  end
+
+  def update_product(
+        %Product{} = product,
+        params
+      ) do
+    product
+    |> Product.changeset(params)
+    |> Repo.update()
+  end
+
+  def delete_product(%Product{} = product) do
+    Repo.delete(product)
+  end
+
+  # --------------------------------Sale--------------------------------
+
+  def list_sales(store_id) do
+    from(s in Sale, where: s.store_id == ^store_id)
+    |> Repo.all()
+  end
+
+  def get_sale(store_id, id) do
+    result =
+      from(s in Sale, where: s.store_id == ^store_id)
+      |> Repo.get(id)
+
+    case result do
+      nil -> {:error, "Sale not found for the given id: #{id}"}
+      sale -> {:ok, sale}
+    end
   end
 end
