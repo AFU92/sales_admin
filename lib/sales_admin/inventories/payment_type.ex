808afd3919910b0514327eb19a75c99e24938299
Andrea Fuentes <andreita_f92@hotmail.com>
2019-02-03 01:16:13 -0500

    PaymentType and StoreType Changesets

--- lib/sales_admin/inventories/payment_type.ex
+++ lib/sales_admin/inventories/payment_type.ex
@@ -1,6 +1,8 @@
 defmodule SalesAdmin.Inventories.PaymentType do
   use Ecto.Schema
-  alias SalesAdmin.Inventories.Sale
+  alias SalesAdmin.Inventories.{Sale, PaymentType}
+
+  import Ecto.Changeset
 
   schema "payment_types" do
     field(:name, :string)
@@ -9,5 +11,19 @@ defmodule SalesAdmin.Inventories.PaymentType do
     has_many(:sales, Sale)
 
     timestamps(type: :utc_datetime)
+  end
+
+  @attrs [:name]
+  @required_attrs [:name]
+
+  @spec changeset(
+          %PaymentType{},
+          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
+        ) :: Ecto.Changeset.t()
+  def changeset(%PaymentType{} = payment_type, params \\ %{}) do
+    payment_type
+    |> cast(params, @attrs)
+    |> validate_required(@required_attrs)
+    |> unique_constraint(:email, name: :payment_types_name_index)
   end
 end
